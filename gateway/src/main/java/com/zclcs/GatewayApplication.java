cd051fa4741963815651b2113c4829b93c18e532
zclcs <2371219112@qq.com>
2022-07-05 15:30:21 +0800

    文件提交

--- gateway/src/main/java/com/zclcs/GatewayApplication.java
+++ gateway/src/main/java/com/zclcs/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.zclcs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author zclcs
  */
 @SpringBootApplication
+@MapperScan("com.zclcs.gateway.mapper")
 public class GatewayApplication {
 
     public static void main(String[] args) {
--- gateway/src/main/java/com/zclcs/gateway/configure/MyRouteEnhanceConfigure.java
+++ gateway/src/main/java/com/zclcs/gateway/configure/MyRouteEnhanceConfigure.java
@@ -5,7 +5,6 @@ import com.zclcs.gateway.runner.MyRouteEnhanceRunner;
 import com.zclcs.gateway.service.BlackListService;
 import com.zclcs.gateway.service.RateLimitRuleService;
 import com.zclcs.gateway.service.RouteEnhanceCacheService;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +18,6 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @EnableAsync
 @Configuration
-@MapperScan("com.zclcs.gateway.mapper")
 public class MyRouteEnhanceConfigure {
 
     @Bean(MyConstant.ASYNC_POOL)
