e9210b482355b42b4844f86616efa5ce49dc2a7b
zclcs <2371219112@qq.com>
2023-03-28 08:53:38 +0800

    3.0 17 重构 TODO 1 docker prod 2 file 管理 3 代码生成 4 redis管理界面集成 5 rabbitmq 管理界面集成 6 分布式定时任务

--- platform/platform-gateway/src/main/resources/sql/initial/1.0.0-procedure.sql
+++ platform/platform-gateway/src/main/resources/sql/initial/1.0.0-procedure.sql
@@ -24,7 +24,7 @@ END;//
 
 drop procedure if exists change_column_if_exists;
 //
--- 修改表字段名称，例如给 system_user 修改字段 code4为code_name，字段类型为int，
+-- 修改表字段，例如给 system_user 修改字段 code4为code_name，字段类型为int，
 -- call change_column_if_exists(database(), 'system_user', 'code4','code_name', 'int not null COMMENT "中国"');//
 create procedure change_column_if_exists(IN dbName tinytext,
                                          IN tableName tinytext,
@@ -74,13 +74,13 @@ END;//
 DROP PROCEDURE IF EXISTS add_index_if_not_exists;
 //
 --  添加表的索引 举例：比如想要在数据库system_user这张表的字段name创建普通索引 nk_system_user_name
---  call add_index_if_not_exists(database(), 'system_user', 1, 'nk_system_user_name', '"name"')
+--  call add_index_if_not_exists(database(), 'system_user', 1, 'nk_system_user_name', 'name')
 --  indexType 1 普通索引 nk 2 唯一索引 uk 3 全文索引 fk 4 空间索引 sk（创建空间索引的列，必须将其声明为NOT NULL，空间索引只能在存储引擎为MYISAM的表）
 CREATE PROCEDURE add_index_if_not_exists(IN dbName tinytext,
                                          IN tableName tinytext,
                                          IN indexType int,
-                                         IN indexField text,
-                                         IN indexName tinytext)
+                                         IN indexName tinytext,
+                                         IN indexField text)
 BEGIN
     SET @index_type = '';
     IF (indexType = 2) THEN
@@ -120,14 +120,14 @@ END;//
 DROP PROCEDURE IF EXISTS change_index_if_exists;
 //
 --  添加表的索引 举例：比如想要在数据库system_user这张表的字段name的普通索引 nk_system_user_name 修改为字段 name2的普通索引 nk_system_user_name2
---  call change_index_if_exists(database(), 'system_user', 1, 'nk_system_user_name2', 'nk_system_user_name', '"name2"', '"name"')
+--  call change_index_if_exists(database(), 'system_user', 1, 'nk_system_user_name2', 'name', 'nk_system_user_name', 'name2')
 --  indexType 1 普通索引 nk 2 唯一索引 uk 3 全文索引 fk 4 空间索引 sk（创建空间索引的列，必须将其声明为NOT NULL，空间索引只能在存储引擎为MYISAM的表）
 CREATE PROCEDURE change_index_if_exists(IN dbName tinytext,
                                         IN tableName tinytext,
                                         IN indexType tinytext,
                                         IN indexName tinytext,
+                                        IN indexField tinytext,
                                         IN oldIndexName text,
-                                        IN indexField text,
                                         IN oldIndexField text)
 BEGIN
     SET @str = concat(
@@ -186,9 +186,8 @@ END;//
 
 DROP PROCEDURE IF EXISTS drop_index_if_exists;
 //
---  添加表的索引 举例：比如想要在数据库system_user这张表的字段name删除普通索引 nk_system_user_name
---  call drop_index_if_exists(database(), 'system_user', 'nk_system_user_name', '"name"')
---  indexType 1 普通索引 nk 2 唯一索引 uk 3 全文索引 fk 4 空间索引 sk（创建空间索引的列，必须将其声明为NOT NULL，空间索引只能在存储引擎为MYISAM的表）
+--  删除表的索引 举例：比如想要在数据库system_user这张表的字段name删除普通索引 nk_system_user_name
+--  call drop_index_if_exists(database(), 'system_user', 'nk_system_user_name', 'name')
 CREATE PROCEDURE drop_index_if_exists(IN dbName tinytext,
                                       IN tableName tinytext,
                                       IN indexName tinytext,
@@ -217,7 +216,8 @@ END;//
 
 
 DROP PROCEDURE IF EXISTS insert_if_not_exists;//
-
+-- 如果数据不存在则新增数据，例如给 system_user 新增数据username为admin
+-- call change_table_name_if_exists(database(), 'system_user', 'username', 'admin', 'username=admin');//
 CREATE PROCEDURE insert_if_not_exists(IN dbName tinytext,
                                       IN tableName tinytext,
                                       IN columnName text,
@@ -244,4 +244,26 @@ BEGIN
         );
     PREPARE stmt FROM @str;
     EXECUTE stmt;
+END;//
+
+drop procedure if exists change_table_name_if_exists;
+//
+-- 修改表名称，例如给 system_user 修改表名为 system_user_2
+-- call change_table_name_if_exists(database(), 'system_user', 'system_user_2');//
+create procedure change_table_name_if_exists(IN dbName tinytext,
+                                         IN oldTableName tinytext,
+                                         IN tableName tinytext)
+begin
+    IF
+        (NOT EXISTS(
+                SELECT *
+                FROM information_schema.TABLES
+                WHERE table_name = oldTableName
+                  and table_schema = dbName
+            ))
+    THEN
+        set @ddl = CONCAT('ALTER TABLE ', dbName, '.', oldTableName, ' RENAME TO ', tableName);
+        prepare stmt from @ddl;
+        execute stmt;
+    END IF;
 END;//
