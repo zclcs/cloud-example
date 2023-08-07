ff792d152fa67f4068e0e37da3ed21ae0e194d0f
zclcs <2371219112@qq.com>
2023-08-07 15:18:45 +0800

    mysql 连接串修复 修复登录不返回密码

--- platform/platform-system-bom/platform-system-api/src/main/java/com/zclcs/platform/system/api/bean/entity/User.java
+++ platform/platform-system-bom/platform-system-api/src/main/java/com/zclcs/platform/system/api/bean/entity/User.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.zclcs.cloud.lib.core.base.BaseEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -49,6 +50,7 @@ public class User extends BaseEntity implements Serializable {
     /**
      * 密码
      */
+    @JsonIgnore
     @TableField("password")
     private String password;
 
