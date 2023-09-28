31aabc0508d07f14b9d6ae348191450ae7aa1e2e
zclcs <2371219112@qq.com>
2023-09-28 14:46:53 +0800

    native compiler

--- cloud-lib/cloud-lib-core/src/main/java/com/zclcs/cloud/lib/core/configure/MyCoreAutoConfigure.java
+++ cloud-lib/cloud-lib-core/src/main/java/com/zclcs/cloud/lib/core/configure/MyCoreAutoConfigure.java
@@ -1,12 +1,18 @@
 package com.zclcs.cloud.lib.core.configure;
 
+import cn.hutool.core.util.ClassUtil;
 import com.zclcs.cloud.lib.core.properties.GlobalProperties;
 import com.zclcs.cloud.lib.core.properties.MyNacosProperties;
 import com.zclcs.cloud.lib.core.properties.RabbitmqApiInfoProperties;
 import com.zclcs.cloud.lib.core.properties.SaTokenProperties;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ImportRuntimeHints;
+
+import java.util.Set;
 
 /**
  * @author zclcs
@@ -18,6 +24,22 @@ import org.springframework.context.annotation.ImportRuntimeHints;
         RabbitmqApiInfoProperties.class,
         SaTokenProperties.class
 })
-@ImportRuntimeHints({CoreRuntimeHintsRegistrar.class})
+@ImportRuntimeHints({MyCoreAutoConfigure.CoreRuntimeHintsRegistrar.class})
 public class MyCoreAutoConfigure {
+
+    static class CoreRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            Set<Class<?>> classes = ClassUtil.scanPackage("com.zclcs");
+            for (Class<?> aClass : classes) {
+                hints.reflection().registerType(aClass, MemberCategory.values());
+            }
+            hints.resources()
+                    .registerPattern("mapper/*")
+                    .registerPattern("generator/templates/*")
+                    .registerPattern("templates/*")
+                    .registerPattern("sql/*")
+            ;
+        }
+    }
 }
--- common/common-redis-starter/src/main/java/com/zclcs/common/redis/starter/service/RedisService.java
+++ common/common-redis-starter/src/main/java/com/zclcs/common/redis/starter/service/RedisService.java
@@ -42,7 +42,6 @@ public class RedisService {
             }
             return true;
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
             return false;
         }
     }
